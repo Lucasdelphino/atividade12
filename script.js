e1434aa41e650515374c5696ee52e247053453d0
Lucasdelphino <80285529+Lucasdelphino@users.noreply.github.com>
2022-09-19 20:13:02 -0300

    atv12

---
+++ script.js
@@ -0,0 +1,16 @@
+// b = 0
+// function slideshow() {
+//     switch (b) {
+//         case 0: FontFaceSetLoadEvent.src = "1.jpg";
+//             break;
+//         case 1: FontFaceSetLoadEvent.src = "2.jpg";
+//             break;
+//         case 2: FontFaceSetLoadEvent.src = "3.jpg";
+//             break;
+//         case 3: FontFaceSetLoadEvent.src = "4.jpg";
+//             break;
+//         case 4: FontFaceSetLoadEvent.src = "5.png";
+//             b = -1;
+//     }
+//     b++;
+// }
